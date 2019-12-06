521976128b85ee1904e7e8f8260a4f26305955bb
Alla Akiyeva <alla.akiyeva@gmail.com>
2019-12-06 10:27:30 -0500

    adding an error message in case server is not connected

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import InstaService from '../services/instaService';
-// import Post from './Post';
+import User from '../components/User';
+import ErrorMessage from './Error';
 
 export default class Posts extends Component{
     InstaService = new InstaService(); //чтобы этот класс вызвать,  используем оператор new
@@ -9,23 +10,65 @@ export default class Posts extends Component{
         error: false
     }
     
+    componentDidMount() {
+        this.updatePosts();
+    }
+
     updatePosts() {
         this.InstaService.getAllPosts()
-        .then()
-        .catch();
+        .then(this.onPostsLoaded) 
+        .catch(this.onError);
     }
 
     onPostsLoaded = (posts) => {
         //this.state.posts = posts; // это грубая ошибка, нельзя напрямую обращаться к стейту
+        this.setState({
+            posts,
+            error: false
+        })
+        console.log(this.state.posts);
+    }
+
+    onError = (err) => {
+        this.setState({
+            error: true
+        })
+    }
+
+    renderItems(arr) {
+        return arr.map( item => { //метод перебора массивов .map(). Этот метод видоизменяет массив. Look up definition of callback functions
+            const {name, altname, photo, src, alt, descr, id} = item; //деструктуризация в ES6. Unrelated: Json-server запускает json файл в виде локального сервера. 
+            return (
+                <div className="post" key={id}>
+                    <User   
+                        src={photo} 
+                        alt={altname} 
+                        name={name}
+                        min
+                    />
+                    <img src={src} alt={alt}></img>
+                    <div className="post__name">
+                        {name}
+                    </div>
+                    <div className="post__descr">
+                        {descr}
+                    </div>
+                </div>
+            )
+        });
     }
 
     render() {
+        const {error, posts} =this.state;
+
+        if (error) {
+            return <ErrorMessage />
+        }
+
+        const items = this.renderItems(posts);
         return (
             <div className="left">
-                {/* <Post src="https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="inst"/>
-                <Post src="https://sadanduseless.b-cdn.net/wp-content/uploads/2018/01/rabbit1.jpg" alt="second" /> */}
-
-
+                {items}
             </div>
         )
     }
